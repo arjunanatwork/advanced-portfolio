70d9757f8afb6f34d85de0a0807a4be8037551e8
Arjunan Ravi <arjunanatwork@gmail.com>
2019-05-19 17:47:14 +0400

    Resetting Contact Fields

--- js/main.js
+++ js/main.js
@@ -24,14 +24,22 @@ $(document).ready(function () {
     });
 
     $('#send-email').click(() => {
-        var template_params = {
-            "from_name": $('#from-name').val(),
-            "from_email": $('#from-email').val(),
-            "message_html": $('#from-msg').val()
+        try {
+            var template_params = {
+                "from_name": $('#from-name').val(),
+                "from_email": $('#from-email').val(),
+                "message_html": $('#from-msg').val()
+            }
+            var service_id = "gmail";
+            var template_id = "template_xHYgEs7V";
+            emailjs.send(service_id, template_id, template_params);
+        } catch (err) {
+            console.error("Error while sending email")
+        } finally {
+            $('#from-name').val('');
+            $('#from-email').val('');
+            $('#from-msg').val('');
         }
-        var service_id = "gmail";
-        var template_id = "template_xHYgEs7V";
-        emailjs.send(service_id, template_id, template_params);
     })
 
 });
